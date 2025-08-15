0933f5e36aed47a03b3a5ad6492d14f6bf70f4ef
Sean <southcarolinadad@gmail.com>
2025-08-15 17:56:50 -0400

    Pattern change line 820

--- app.py
+++ app.py
@@ -817,7 +817,7 @@ def signup_page():
     '''
     
 def is_valid_email(email):
-    pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}
+    pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
     
     return re.match(pattern, email) is not None
     
